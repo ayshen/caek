940bb33e101d9b8d7199627cf7f5ffebb10e2036
Andrew Shen <ayshen@ucsd.edu>
2013-08-11 23:46:24 -0700

    Add some javadoc.

--- src/com/example/aperture/core/IModule.aidl
+++ src/com/example/aperture/core/IModule.aidl
@@ -3,6 +3,18 @@ package com.example.aperture.core;
 import android.content.Intent;
 
 
+/** The communication interface between a requesting context and a module.
+*/
 interface IModule {
+
+    /** Query a module for relevant actions based on an input.
+    @param data An Intent containing a content query (plain text, image, video,
+    ...)
+    @return A List of well-formed Intents, each of which should be immediately
+    usable in <code>Context.startActivity()</code>, representing actions that
+    are relevant to the content query in the scope of what the module
+    understands. See {@link com.example.aperture.core.Module} for keys for
+    extras that can be added to the individual Intents for prettier display.
+    */
     List<Intent> process(in Intent data);
 }
--- src/com/example/aperture/core/ModuleManagement.java
+++ src/com/example/aperture/core/ModuleManagement.java
@@ -1,11 +1,22 @@
 package com.example.aperture.core;
 
 
+/** Constants for dealing with installed modules.
+Used by {@link com.example.aperture.core.StalemateResolutionActivity} and
+{@link com.example.aperture.core.ModuleHost} to handle changes in the installed
+modules and connections to modules respectively.
+*/
 public class ModuleManagement {
-    //public final static String PREFERENCES_NAME =
-    //        "com.example.aperture.core.SHARED_PREFERENCES";
+
+    /** Name of shared preferences key for the list of installed modules the
+    last time {@link com.example.aperture.core.StalemateResolutionActivity} was
+    started.
+    */
     public final static String LAST_ALL_MODULES =
             "com.example.aperture.core.LAST_ALL_MODULES";
+
+    /** Action used with an Intent for binding to a module.
+    */
     public final static String ACTION_BIND_MODULE =
             "com.example.aperture.core.ACTION_BIND_MODULE";
 }
